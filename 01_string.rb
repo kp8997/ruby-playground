6c98f087933d3bed3dfb4dc948a427a2e14bc3e0
Khang Phan <khangphan@Khangs-MacBook-Pro.local>
2022-12-15 14:31:50 +0700

    initialize

---
+++ 01_string.rb
@@ -0,0 +1,17 @@
+print "hello world"
+puts "hello world"
+
+x = 10
+y = "test"
+
+puts y
+print "#{x} \n" + "\n" + "\n" + "\n" + "\n"
+
+
+def says(str)
+	puts str
+end
+
+says "wack"
+
+print y.capitalize + "\n"
---
+++ 02_chomp.rb
@@ -0,0 +1,4 @@
+puts "Enter a number and I will multiply it by 2"
+input = gets.chomp
+puts input.to_i * 2
+ 
