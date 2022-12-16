2ce6f1566a6f74faf5e4d2a1a07710da223dd1ab
Khang Phan <khang.phan@employmenthero.com>
2022-12-16 15:48:02 +0700

    add more method related to hash

--- 10_hash.rb
+++ 10_hash.rb
@@ -9,3 +9,10 @@ p hash['a']
 hash.each do |key, value|
   puts "#{key}: #{value}"
 end
+
+h = {"test" => 13, "check" => 14}
+h1 = {"test" => 13, "check" => 14, "l" => "Mushroom", "c" => "Chicken"} 
+
+h1.each { |k, v|  h1.delete(k) if v.is_a?(String) }
+  p h1
+# h.keys, h.values 
