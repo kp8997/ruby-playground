44e0f011c0cc5dd1ab5ae6fa6422febbeff4ece1
Khang Phan <khang.phan@employmenthero.com>
2022-12-19 17:04:51 +0700

    update some methof and accessor

--- oop/student.rb
+++ oop/student.rb
@@ -1,4 +1,9 @@
 class Student
+
+  attr_accessor :first_name, :last_name, :email, :username, :password
+  # attr_reader
+  # attr_writer
+
   @first_name
   @last_name
   @email
@@ -6,38 +11,36 @@ class Student
   @password 
 
   # constructor
-  # def initialize(first_name, last_name)
-  #   @first_name = first_name
-  #   @last_name = last_name
-  # end
+  def initialize(first_name, last_name, email, username, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @username = username
+    @password = password
+  end
 
   # getter
-  def first_name
-    @first_name
-  end
+  # def first_name
+  #   @first_name
+  # end
 
-  def last_name
-    @last_name
-  end
+  # def last_name
+  #   @last_name
+  # end
 
   #  setter
-  def first_name=(name) 
-    @first_name = name
-  end
+  # def first_name=(name)
+  #   @first_name = name
+  # end
 
-  def last_name=(name)
-    @last_name = name
-  end
+  # def last_name=(name)
+  #   @last_name = name
+  # end
 
   def to_s()
     "Fullname is #{@first_name} #{@last_name}"
   end
 end
 
-
-tom = Student.new
-tom.first_name = 'Tom'
-tom.last_name = 'Bettercourt'
+tom = Student.new('Tom', 'Bettercourt', 'bettercourt1@example.com', 'beer1', 'password')
 puts tom
-puts tom.first_name
-puts tom.last_name
