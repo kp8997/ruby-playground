5121e506fb34661ca067ea32640acc591e7450ec
Khang Phan <khang.phan@employmenthero.com>
2022-12-28 17:26:07 +0700

    create new exercise

--- alpha-blog/app/controllers/exercises_controller.rb
+++ alpha-blog/app/controllers/exercises_controller.rb
@@ -8,4 +8,20 @@ class ExercisesController < ApplicationController
   def show
     
   end
+
+  def index
+    @exercises = Exercise.all()
+  end
+
+  def new
+   
+  end
+
+  def create
+    # params[:exercise]
+    # render plain: params['exercise']
+    # @exercise = Exercise.new(params[:exercise])
+    @exercise = Exercise.new(params.require(:exercise).permit(:question, :answer, :create_at, :update_at))
+    render plain: @exercise.inspect
+  end
 end
--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'exercises/:id', to: 'exercises#fetch'
+  # get 'exercises/:id', to: 'exercises#fetch'
   resources :articles, only: [:show]
+  resources :exercises, only: [:index, :new, :create]
+  # resources :exercises # equal to CRUD for Exercise class  model
   # resources :exercises, only: [:show, :index, :new, :edit, :create, :update, :destroy]
 end
