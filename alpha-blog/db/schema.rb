74655891edd3a51f94fbda575597114c944b26b6
Khang Phan <khang.phan@employmenthero.com>
2023-01-06 15:45:33 +0700

    setup migration

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_28_071959) do
+ActiveRecord::Schema.define(version: 2023_01_06_071355) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2022_12_28_071959) do
   create_table "exercises", force: :cascade do |t|
     t.string "question"
     t.text "answer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
