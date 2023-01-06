dad5563d88cd02d8fd619206478430d81a6b3aac
Khang Phan <khang.phan@employmenthero.com>
2023-01-06 17:24:15 +0700

    add validation for username and email user model

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_06_071355) do
+ActiveRecord::Schema.define(version: 2023_01_06_085217) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2023_01_06_071355) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
